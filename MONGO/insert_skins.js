34f93d6ff0818f9d53d0d15b6d7209d0e58d4f2f
jaume_pm <jaumepetit2002@gmail.com>
2023-10-31 11:49:37 +0100

    Create volume for data to be kept after the container shutdown.
    Also delete id in skin, as we will use the implicit _id field of mongo to identify each skin.

--- MONGO/insert_skins.js
+++ MONGO/insert_skins.js
@@ -5,35 +5,30 @@ var db = conn.getDB("J2D"); // Change to your database name
 // Skins data to be inserted
 var skinsToInsert = [
     {
-        id: 1,
         name: "Skin A",
         price: 25.99,
         color: "Red",
         rarity: "Rare"
     },
     {
-        id: 2,
         name: "Skin B",
         price: 19.50,
         color: "Blue",
         rarity: "Common"
     },
     {
-        id: 3,
         name: "Skin C",
         price: 30.75,
         color: "Green",
         rarity: "Epic"
     },
     {
-        id: 4,
         name: "Skin D",
         price: 15.99,
         color: "Purple",
         rarity: "Uncommon"
     },
     {
-        id: 5,
         name: "Skin E",
         price: 22.49,
         color: "Gold",
