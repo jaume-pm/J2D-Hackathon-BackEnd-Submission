7d331fcb22d9bb241b7852577fa4abf5540a9f41
jaume_pm <jaumepetit2002@gmail.com>
2023-11-05 16:51:41 +0100

    Add code comments

--- API/controllers/controller_skins.py
+++ API/controllers/controller_skins.py
@@ -1,24 +1,18 @@
 from flask import jsonify, request
+from bson.objectid import ObjectId
 from models.models import *  # Importing necessary models
-from bson.objectid import ObjectId  # Importing ObjectId from bson module
 from utilities.getters import *  # Importing custom getter functions
 from utilities.economy import *
 
 def get_skins_available():
-    """
-    Retrieves available skins from the MongoDB collection and returns them as a JSON response.
-
-    Returns:
-        JSON: {"result": "ok", "skins": available_skins}
-    """
     try:
-        # Retrieve all skins from the MongoDB collection
+        # Retrieve available skins from the MongoDB collection
         available_skins = list(skins.find({}))
-
+        
         # Convert ObjectId to string for each document
         for skin in available_skins:
             skin['_id'] = str(skin['_id'])
-
+        
         # Return the list of available skins as a JSON response
         return jsonify({"result": "ok", "skins": available_skins}), 200
 
@@ -28,12 +22,6 @@ def get_skins_available():
 
 
 def add_skins():
-    """
-    Adds skins to the MongoDB collection based on the provided JSON file in the request.
-
-    Returns:
-        JSON: {"result": "ok"} if successful, {"error": str(e)} if there's an error.
-    """
     try:
         # Check if the request contains a JSON file
         if 'file' not in request.files or request.files['file'].filename == '':
@@ -55,14 +43,6 @@ def add_skins():
 
 
 def get_user_skins_info():
-    """
-    Retrieves information about skins owned by a user from the MongoDB collection.
-
-    Returns:
-        JSON: {"result": "ok", "skins": user_skin_info} if successful,
-              {"error": f"No user found with ID: {user_id}"} if the user is not found,
-              {"error": str(e)} if there's an error.
-    """
     try:
         # Retrieve user ID from the JSON data in the request
         json_data = request.get_json()
@@ -71,42 +51,39 @@ def get_user_skins_info():
         # Find the user in the MongoDB collection
         user = users.find_one({"_id": user_id})
 
-        if user:
-            # Extract information about skins owned by the user
-            user_owned_skins = user.get("owned_skins", [])
-            user_skin_info = []
-
-            for owned_skin in user_owned_skins:
-                skin_id = owned_skin.get("skin_id")
-                skin = skins.find_one({"_id": skin_id})
-                if skin:
-                    user_skin_info.append({
-                        "_id": str(skin_id),
-                        "name": skin.get("name"),
-                        "color": owned_skin.get("color"),
-                        "rarity": skin.get("rarity")
-                    })
-
-            # Return the information about user-owned skins as a JSON response
-            return jsonify({"result": "ok", "skins": user_skin_info}), 200
-
-        else:
-            # Return an error response if the user is not found
+        # Return an error response if the user is not found
+        if not user:
             return jsonify({"error": f"No user found with ID: {user_id}"}), 404
 
+        # Extract information about skins owned by the user
+        user_owned_skins = user.get("owned_skins", [])
+        user_skin_info = []
+
+        for owned_skin in user_owned_skins:
+            skin_id = owned_skin.get("skin_id")
+            skin = skins.find_one({"_id": skin_id})
+            
+            # Add skin information to the user_skin_info list
+            if skin:
+                user_skin_info.append({
+                    "_id": str(skin_id),
+                    "name": skin.get("name"),
+                    "color": owned_skin.get("color"),
+                    "rarity": skin.get("rarity")
+                })
+
+        # Return the information about user-owned skins as a JSON response
+        return jsonify({"result": "ok", "skins": user_skin_info}), 200
+
+    except ValidationError as e:
+        # Handle validation errors and return an error response
+        return jsonify({"error": str(e)}), 400
     except Exception as e:
         # Handle exceptions and return an error response
         return jsonify({"error": str(e)}), 500
 
-def change_skin_color():
-    """
-    Changes the color of a skin owned by a user in the MongoDB collection.
 
-    Returns:
-        JSON: {"result": "ok"} if successful,
-              {"error": "No matching skin found"} if no matching skin is found,
-              {"error": str(e)} if there's an error.
-    """
+def change_skin_color():
     try:
         # Retrieve data from the JSON data in the request
         json_data = request.get_json()
@@ -117,27 +94,27 @@ def change_skin_color():
         # Find the user in the MongoDB collection
         user = users.find_one({"_id": user_id})
 
-        if user:
-            try:
-                # Update the color of the specified skin for the user
-                result = users.update_one(
-                    {'_id': user_id, 'owned_skins.skin': skin_id},
-                    {'$set': {'owned_skins.$.color': color}}
-                )
-
-                if result.matched_count == 0:
-                    # Return an error response if no matching skin is found
-                    return jsonify({"error": "No matching skin found"}), 404
-                else:
-                    # Return a success response if the operation is successful
-                    return jsonify({"result": "ok"}), 200
-
-            except Exception as e:
-                # Handle exceptions related to the update operation
-                return jsonify({"error": f"Error updating skin color: {str(e)}"}), 500
-        else:
-            # Return an error response if the user is not found
+        # Return an error response if the user is not found
+        if not user:
             return jsonify({"error": f"No user found with ID: {user_id}"}), 404
+        
+        try:
+            # Update the color of the specified skin for the user
+            result = users.update_one(
+                {'_id': user_id, 'owned_skins.skin_id': ObjectId(skin_id)},
+                {'$set': {'owned_skins.$.color': color}}
+            )
+
+            # Return an error response if no matching skin is found
+            if result.matched_count == 0:
+                return jsonify({"error": "No matching skin found"}), 404
+            else:
+                # Return a success response if the operation is successful
+                return jsonify({"result": "ok"}), 200
+
+        except Exception as e:
+            # Handle exceptions related to the update operation
+            return jsonify({"error": f"Error updating skin color: {str(e)}"}), 500
 
     except ValidationError as e:
         # Handle validation errors and return an error response
@@ -145,109 +122,114 @@ def change_skin_color():
     except Exception as e:
         # Handle unexpected exceptions and return an error response
         return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
-    
+
 
 def sell_skin(skin_id):
     try:
+        # Retrieve data from the JSON data in the request
         json_data = request.get_json()
         user_id = get_user_id(json_data)
         sell_price = get_sell_price(skin_id)
 
+        # Find the user in the MongoDB collection
         user = users.find_one({"_id": user_id})
 
-        if user:
-            try:
-                user_balance = user.get("balance")
-
-                print(f"Attempting to sell skin_id: {skin_id} for user_id: {user_id}")
-
-                result = users.update_one(
-                    {'_id': user_id, 'owned_skins.skin': ObjectId(skin_id)},
-                    {
-                        '$set': {'balance': user_balance + sell_price},
-                        '$pull': {'owned_skins': {'skin': ObjectId(skin_id)}}
-                    }
-                )
-
-                if result.matched_count == 0:
-                    return jsonify({"error": "The user does not own the skin"}), 404
-                else:
-                    return jsonify({"result": "ok"}), 200
-
-            except Exception as e:
-                return jsonify({"error": f"Error selling skin: {str(e)}"}), 500
-        else:
+        # Return an error response if the user is not found
+        if not user:
             return jsonify({"error": f"No user found with ID: {user_id}"}), 404
 
+        try:
+            user_balance = user.get("balance")
+
+            # Attempt to sell the specified skin for the user
+            result = users.update_one(
+                {'_id': user_id, 'owned_skins.skin_id': ObjectId(skin_id)},
+                {
+                    '$set': {'balance': user_balance + sell_price},
+                    '$pull': {'owned_skins': {'skin_id': ObjectId(skin_id)}}
+                }
+            )
+
+            # Return an error response if no matching skin is found
+            if result.matched_count == 0:
+                return jsonify({"error": "The user does not own the skin"}), 404
+            else:
+                # Return a success response if the operation is successful
+                return jsonify({"result": "ok"}), 200
+
+        except Exception as e:
+            # Handle exceptions related to selling the skin
+            return jsonify({"error": f"Error selling skin: {str(e)}"}), 500
+
     except ValidationError as e:
+        # Handle validation errors and return an error response
         return jsonify({"error": str(e)}), 400
     except Exception as e:
+        # Handle unexpected exceptions and return an error response
         return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
+
 
 def get_skin_info(skin_id):
     try:
-        # Convert the skin_id string to ObjectId
-        skin_id_obj = ObjectId(skin_id)
+        # Find the skin with the provided ID in the MongoDB collection
+        skin = skins.find_one({"_id": ObjectId(skin_id)})
 
-        # Find the skin with the provided ID
-        skin = skins.find_one({"_id": skin_id_obj})
-
-        # Check if the skin exists
-        if skin:
-            # Convert ObjectId to string for the "_id" field
-            skin["_id"] = str(skin["_id"])
-
-            # Return all information about the skin as a JSON response
-            return jsonify({"result": "ok", "skin": skin}), 200
-        else:
-            # Return an error response if the skin is not found
+        # Return an error response if the skin is not found
+        if not skin:
             return jsonify({"error": f"No skin found with ID: {skin_id}"}), 404
+
+        # Convert ObjectId to string for the "_id" field
+        skin["_id"] = str(skin["_id"])
+
+        # Return all information about the skin as a JSON response
+        return jsonify({"result": "ok", "skin": skin}), 200
 
     except Exception as e:
         # Handle exceptions and return an error response
         return jsonify({"error": str(e)}), 500
 
 
-
 def buy_skin():
     try:
+        # Retrieve data from the JSON data in the request
         json_data = request.get_json()
         user_id = get_user_id(json_data)
         skin_id = get_skin_id(json_data)
 
+        # Find the user in the MongoDB collection
         user = users.find_one({"_id": user_id})
 
-        if user:
-            try:
-                # Check if user already has the skin
-                if any(owned_skin['skin_id'] == skin_id for owned_skin in user.get('owned_skins', [])):
-                    return jsonify({"error": "User already owns the skin"}), 400
-
-                # Get the skin details
-                skin = skins.find_one({"_id": skin_id})
-
-                # Check if the user has enough balance to buy the skin
-                skin_price = skin.get("price")
-                user_balance = user.get("balance")
-                if user_balance < skin_price:
-                    return jsonify({"error": "Insufficient balance"}), 400
-
-                # Deduct the skin price from the user's balance and add the skin to owned_skins
-                new_balance = user_balance - skin_price
-
-                # Update the user's document in the database
-                users.update_one(
-                    {"_id": user_id},
-                    {"$inc": {"balance": -skin_price}, "$push": {"owned_skins": {"skin_id": skin_id, "color": skin.get("color")}}}
-                )
-
-                return jsonify({"message": "Skin purchased successfully"}), 200
-
-            except Exception as e:
-                return jsonify({"error": f"Error buying skin: {str(e)}"}), 500
-        else:
+        # Return an error response if the user is not found
+        if not user:
             return jsonify({"error": f"No user found with ID: {user_id}"}), 404
 
+        try:
+            # Check if user already owns the skin
+            if any(owned_skin['skin_id'] == skin_id for owned_skin in user.get('owned_skins', [])):
+                return jsonify({"error": "User already owns the skin"}), 400
+
+            # Find the skin details in the MongoDB collection
+            skin = skins.find_one({"_id": ObjectId(skin_id)})
+
+            # Check if the user has enough balance to buy the skin
+            skin_price = skin.get("price")
+            user_balance = user.get("balance")
+            if user_balance < skin_price:
+                return jsonify({"error": "Insufficient balance"}), 400
+
+            # Deduct the skin price from the user's balance and add the skin to owned_skins
+            users.update_one(
+                {"_id": user_id},
+                {"$inc": {"balance": -skin_price}, "$push": {"owned_skins": {"skin_id": ObjectId(skin_id), "color": skin.get("color")}}}
+            )
+
+            # Return a success response
+            return jsonify({"message": "Skin purchased successfully"}), 200
+
+        except Exception as e:
+            # Handle exceptions related to buying the skin
+            return jsonify({"error": f"Error buying skin: {str(e)}"}), 500
+        
     except ValidationError as e:
         # Handle validation errors and return an error response
         return jsonify({"error": str(e)}), 400
--- API/utilities/economy.py
+++ API/utilities/economy.py
@@ -2,18 +2,9 @@ from bson.objectid import ObjectId
 from models.models import skins
 
 # Constant percentage for calculating the sell price
-SELL_PERCENTAGE = 0.6  # 60%
+SELL_PERCENTAGE = 0.6  # 60% of the skin's original price
 
 def get_sell_price(skin_id_str):
-    """
-    Calculate the sell price of a skin based on the constant percentage and the skin's price.
-
-    Args:
-        skin_id_str (str): The string representation of the ObjectId of the skin.
-
-    Returns:
-        float: The calculated sell price.
-    """
     try:
         # Convert the string representation of ObjectId to ObjectId
         skin_id = ObjectId(skin_id_str)
